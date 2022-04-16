328f02505f80a429730efb28f28b0b45d46bb9db
VictoryMichael <98047104+VictoryMichael@users.noreply.github.com>
2022-04-16 16:23:36 +0100

    Add files via upload

---
+++ farm landing page/index.js
@@ -0,0 +1,49 @@
+
+let  menu  = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+} );
+
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+
+}
+
+
+
+var swiper = new Swiper(".image-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+loop:true,
+autoplay:{
+delay:2000,
+disableOnInteraction:false,
+
+
+}
+  });
+
+
+
+
+
+
+
+
+
+
+
